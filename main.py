9bc362d56585e3834a2b02195da79709272a84b2
Sceluswe <emilcbm@protonmail.com>
2020-08-08 02:26:08 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+#
+# Global default settings
+#
+EXIT_SUCCESS = 0
+
+def main():
+    print("Enter nutritional values:")
+    calories = input("Calories:")
+    carbs = input("Carbohydrates:")
+    protein = input("Protein:")
+    fat = input("Fat:")
+
+    print(calories)
+    print(carbs)
+    print(protein)
+    print(fat)
+    sys.exit(EXIT_SUCCESS)
+
+main()
